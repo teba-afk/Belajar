f53812c92d0db8016186ceeaa892c42f6d14bafb
teba-afk <realme2gaboga@gmail.com>
2025-05-24 15:28:54 +0800

    video.js

--- channels/kisal/video.js
+++ channels/kisal/video.js
@@ -1,18 +1,72 @@
-window.onload = function () {
-  const manifestUri = "https://unifi-live07.secureswiftcontent.com/UnifiHD/live06.mpd";
+const manifestUri = 'https://unifi-live07.secureswiftcontent.com/UnifiHD/live06.mpd';
 
-  shaka.polyfill.installAll();
+    function initApp() {
+      shaka.polyfill.installAll();
+      if (shaka.Player.isBrowserSupported()) {
+        init();
+      } else {
+        console.error('Browser not supported!');
+      }
+    }
 
-  if (shaka.Player.isBrowserSupported()) {
-    const video = document.getElementById("video");
-    const player = new shaka.Player(video);
+    async function init() {
+      const video = document.getElementById('video');
+      const ui = video['ui'];
+      const controls = ui.getControls();
+      const player = controls.getPlayer();
+      const config = {
+        'seekBarColors': {
+          base: 'rgba(66, 133, 244, 0.35)',
+          buffered: 'rgba(66, 133, 244, 0.6)',
+          played: 'rgba(66, 133, 244, 0.8)',
+        },
+        'volumeBarColors': {
+          base: 'rgba(66, 133, 244, 0.8)',
+          level: 'rgb(66, 133, 244)',
+        }
+      }
+      ui.configure(config);
+      
+      // Configure ClearKey DRM
+            player.configure({
+                drm: {
+                    clearKeys: {
+                        'R6Ega0SCLIkyDnCRbPV7DA': '4EDKKqQm33ibo4S6VhiRtA'
+                    }
+                }
+            });
 
-    player.load(manifestUri).then(function () {
-      console.log("Video loaded!");
-    }).catch(function (e) {
-      console.error("Error loading video", e);
-    });
-  } else {
-    console.error("Browser not supported!");
-  }
-};
+      window.player = player;
+      window.ui = ui;
+      window.controls = controls;
+
+      player.addEventListener("error", onPlayerErrorEvent);
+      controls.addEventListener("error", onUIErrorEvent);
+
+      try {
+        await player.load(manifestUri);
+        console.log('The video has now been loaded!');
+      } catch (error) {
+        onPlayerError(error);
+      }
+
+      function onPlayerErrorEvent(errorEvent) {
+        onPlayerError(event.detail);
+      }
+
+      function onPlayerError(error) {
+        console.error('Error code', error.code + ':', 'Video could not be loaded!', '[Media not found].');
+      }
+
+      function onUIErrorEvent(errorEvent) {
+        onPlayerError(event.detail);
+      }
+    }
+
+    function initFailed(errorEvent) {
+      console.error('Unable to load the UI library!');
+    }
+
+    document.addEventListener('shaka-ui-loaded', init);
+    document.addEventListener('shaka-ui-load-failed', initFailed);
+    document.addEventListener('DOMContentLoaded', initApp);
