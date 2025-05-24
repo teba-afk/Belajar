59393c44d1da6d18d9fd7306bd51fe40233b9b62
teba-afk <realme2gaboga@gmail.com>
2025-05-24 14:47:31 +0800

    video.js

--- channels/kisal/video.js
+++ channels/kisal/video.js
@@ -1,26 +1,18 @@
-const manifestUri = "https://unifi-live07.secureswiftcontent.com/UnifiHD/live06.mpd";
+window.onload = function () {
+  const manifestUri = "https://unifi-live07.secureswiftcontent.com/UnifiHD/live06.mpd";
 
-function initApp() {
   shaka.polyfill.installAll();
+
   if (shaka.Player.isBrowserSupported()) {
-    initPlayer();
+    const video = document.getElementById("video");
+    const player = new shaka.Player(video);
+
+    player.load(manifestUri).then(function () {
+      console.log("Video loaded!");
+    }).catch(function (e) {
+      console.error("Error loading video", e);
+    });
   } else {
     console.error("Browser not supported!");
   }
-}
-
-async function initPlayer() {
-  const video = document.getElementById("video");
-  const player = new shaka.Player(video);
-
-  window.player = player;
-
-  try {
-    await player.load(manifestUri);
-    console.log("The video has now been loaded!");
-  } catch (e) {
-    console.error("Error loading video", e);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", initApp);
+};
